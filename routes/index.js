4811a578baeaf7ca169b1eecc720d85008c2cd97
Ian Behrmann <ianjbehrmann@gmail.com>
2015-11-12 16:16:52 -0700

    get specific year route configured

--- routes/index.js
+++ routes/index.js
@@ -10,7 +10,7 @@ router.post('/api/v1/memories', function(req, res, next) {
     if (err) {
       return console.error('error fetching client from pool', err);
     }
-    client.query('INSERT INTO memories(old_days, these_days, year) VALUES($1,$2,$3)',[req.body.data.attributes.old_days, req.body.data.attributes.these_days, req.body.year], function(err, result) {
+    client.query('INSERT INTO memories(old_days, these_days, year) VALUES($1,$2,$3)',[req.body.data.attributes.old_days, req.body.data.attributes.these_days, req.body.data.attributes.year], function(err, result) {
       done();
       if (err) {
         return console.error('error running query', err);
@@ -38,6 +38,23 @@ router.get('/api/v1/memories', function(req, res, next) {
 
   });
 });
+
+router.get('/api/v1/memories/:year', function (req, res, next){
+  pg.connect(conString, function(err, client, done) {
+
+    if (err) {
+      return console.error('error fetching client from pool', err);
+    }
+    client.query('SELECT * from memories where year = $1',[req.params.year], function(err, result) {
+      done();
+      if (err) {
+        return console.error('error running query', err);
+      }
+      res.json(schema.formatRes(result.rows)).status(200).end()
+    });
+
+  });
+})
 
 
 
